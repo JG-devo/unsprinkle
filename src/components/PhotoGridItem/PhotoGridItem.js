55dc96e5af422f0a05c968e798950cdd7b12251c
Jean Gabriel <67287800+JG-devo@users.noreply.github.com>
2023-07-11 14:23:04 +0200

    Complete project

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -5,10 +5,28 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <Image>
+          <source
+            type="image/avif"
+            srcset={`
+            ${src.replace('.jpg', '.avif')} 1x,
+            ${src.replace('.jpg', '@2x.avif')} 2x,
+            ${src.replace('.jpg', '@3x.avif')} 3x,
+    `}
+          />
+          <source
+            type="image/jpeg"
+            srcset={`
+            ${src} 1x,
+            ${src.replace('.jpg', '@2x.jpg')} 2x,
+            ${src.replace('.jpg', '@3x.jpg')} 3x,
+            `}
+          />
+          <img alt={alt} src={src} />
+        </Image>
       </Anchor>
       <Tags>
-        {tags.map((tag) => (
+        {tags.map(tag => (
           <Tag key={tag}>{tag}</Tag>
         ))}
       </Tags>
@@ -22,26 +40,36 @@ const Anchor = styled.a`
   outline-offset: 4px;
 `;
 
-const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 300px;
-  border-radius: 2px;
-  margin-bottom: 8px;
+const Image = styled.picture`
+  & img {
+    height: 300px;
+    width: 100%;
+    aspect-ratio: 4 / 3;
+    object-fit: cover;
+    display: block;
+    border-radius: 2px;
+    margin-bottom: 8px;
+  }
 `;
 
 const Tags = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
+  display: inline;
   padding: 4px 8px;
   background: var(--color-gray-300);
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+
+  &:not(:last-of-type) {
+    margin-right: 8px;
+  }
 `;
 
 export default PhotoGridItem;
